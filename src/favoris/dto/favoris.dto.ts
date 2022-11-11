b5c53c7045162ada72ae2390a62153db1176147e
Aestorgs <dylan.bo@institutsolacroup.com>
2022-11-11 16:56:55 +0100

    update

--- src/favoris/dto/favoris.dto.ts
+++ src/favoris/dto/favoris.dto.ts
@@ -1,10 +1,8 @@
 import { IsNotEmpty } from 'class-validator';
 import { Users } from 'src/entities/users.entety';
-import { Column } from 'typeorm';
 
 export class CreateFavorisDto {
   @IsNotEmpty()
-  @Column()
   showsId: number;
 
   @IsNotEmpty()
