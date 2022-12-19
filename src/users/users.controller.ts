9d6937b7be987cb2bbd4af96a960ed5514a320fd
Dylan R <90897072+Aestorgs@users.noreply.github.com>
2022-12-19 19:03:13 +0100

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,18 +16,19 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  // pour envoyer les donnée register 
   @Post('register')
   @UsePipes(ValidationPipe)
   postUsers(@Body() createUserDto: CreateUsersDto) {
     return this.usersService.createUsers(createUserDto);
   }
-
+// pour connecter au login 
   @Post('login')
   @HttpCode(200)
   loginUsers(@Body() users: any) {
     return this.usersService.loginUsers(users.email, users.password);
   }
-
+// pour afficher les favoris et id 
   @Get('favoris/:id')
   getUsersFavoris(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findByFavorisId(id);
