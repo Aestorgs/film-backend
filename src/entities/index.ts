21983b4c231f837dd121293c6c7a770e4dd1de65
Dylan R <90897072+Aestorgs@users.noreply.github.com>
2022-12-19 18:52:37 +0100

    Update index.ts

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -4,5 +4,6 @@ import { Saison } from './saison.entity';
 import { Show } from './show.entity';
 import { Users } from './users.entety';
 
+ //j'affiche les tables dans postgres 
 const entities = [Users, Favoris, Saison, Episode, Show];
 export default entities;
